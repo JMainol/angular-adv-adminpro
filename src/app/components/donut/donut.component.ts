2bf9b131027c1b60f51b6880b1e10e11a3dcd3f2
Jose Manuel Charro <jmcharrogarcia@gmail.com>
2020-07-28 14:02:31 +0200

    Input, output y viewchild

---
+++ src/app/components/donut/donut.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+
+import { MultiDataSet, Label, Color } from 'ng2-charts';
+
+@Component({
+  selector: 'app-donut',
+  templateUrl: './donut.component.html',
+  styles: [
+  ]
+})
+export class DonutComponent {
+
+  @Input() title: string = 'Sin Titulo';
+
+  @Input('labels') doughnutChartLabels: Label[] = ['Ganados', 'Empatados', 'Perdidos'];
+  @Input('data') doughnutChartData: MultiDataSet = [
+    [350, 450, 100],
+  ];
+
+  public colors: Color[] = [
+    { backgroundColor: [ '#6857E6', '#009FEE', '#F02059' ] }
+  ];
+
+}
